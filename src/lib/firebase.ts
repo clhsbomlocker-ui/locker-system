ad236b3ee1df9567d0fb8d99550548a815ecb162
Ambert Chan Wye Zhe <m-10307033@moe-dl.edu.my>
2025-11-25 16:48:04 +0800

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBIuTJIj6vkMcerk16Rzll1FtT5ciTZulY",
-  authDomain: "clhs-bom-locker.firebaseapp.com",
-  projectId: "clhs-bom-locker",
-  storageBucket: "clhs-bom-locker.firebasestorage.app",
-  messagingSenderId: "86208175190",
-  appId: "1:86208175190:web:a5ab067120742f85c92ff7",
-  measurementId: "G-CPQGZS6YRS",
-}
+  apiKey: "AIzaSyAODrWCLvQoQs9cwCEKakyoENUeArL21-w",
+  authDomain: "clhsbomlocker-8382b.firebaseapp.com",
+  projectId: "clhsbomlocker-8382b",
+  storageBucket: "clhsbomlocker-8382b.firebasestorage.app",
+  messagingSenderId: "17916640649",
+  appId: "1:17916640649:web:c886c175a817d618383558",
+  measurementId: "G-BFFHFGE3DY"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
